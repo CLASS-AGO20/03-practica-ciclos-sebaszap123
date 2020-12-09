1b70865c226ef1ac9b425b791c5caebb0de6df67
sebaszap123 <mfrausto0@ucol.mx>
2020-12-09 17:12:27 -0600

    added convertir string and obtener divisibles

--- src/main.js
+++ src/main.js
@@ -2,9 +2,29 @@ export default class App {
   factorial(numero) {
     let total = 1;
     for (let i = 1; i <= numero; i++) {
-        total = total * i
+      total = total * i;
     }
-    return total
+    return total;
+  }
+  convertirAString(numero) {
+    let i = 1;
+    let resultado = "";
+    while (i <= numero) {
+      resultado = "*" + resultado;
+      i++;
+    }
+    return resultado;
+  }
+  obtenerDivisibles(numero) {
+    let result = "";
+    let i = "";
+
+    do {
+      i = i + "*";
+      result = result + i;
+    } while (i < numero);
+
+    console.log(result);
   }
 }
 
