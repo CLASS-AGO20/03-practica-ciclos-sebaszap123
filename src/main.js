530a5cf35129dfe6277aa95e01beb8681cd64eef
sebaszap123 <mfrausto0@ucol.mx>
2020-12-09 17:10:30 -0600

    Added factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
-export default class App {}
+export default class App {
+  factorial(numero) {
+    let total = 1;
+    for (let i = 1; i <= numero; i++) {
+        total = total * i
+    }
+    return total
+  }
+}
 
 let app = new App();
+
+console.log(app.factorial(10));
+console.log(app.convertirAString(10))
